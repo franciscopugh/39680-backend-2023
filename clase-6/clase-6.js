4f7ae9c6b25a2d61b153c693a70d12c0de78d52b
Francisco Pugh <franciscopugh01@gmail.com>
2023-01-26 20:06:40 -0300

    Clase 6 B

--- clase-6/clase-6.js
+++ clase-6/clase-6.js
@@ -9,13 +9,13 @@
 
 const crypto = require('crypto')
 
-//console.log(crypto.getCiphers()) Consultar metodos encriptacion
+console.log(crypto.getCiphers().length)
 
 const algoritmo = 'aes-256-cbc'
 const key=  crypto.randomBytes(32)
 const iv = crypto.randomBytes(16)
 
-const hackerman = (password) => {
+const encriptar = (password) => {
     const cipher = crypto.createCipheriv(algoritmo, Buffer.from(key),iv ) //Creacion de objeto Cipher utilizado para el cifrado de datos
     let encriptacion = cipher.update(password) //Metodo que actualiza el cifrado con el valor del parametro
     encriptacion = Buffer.concat([encriptacion, cipher.final()]) //Concatena el valor ingresado con el valor del objeto cifrado 
@@ -25,5 +25,19 @@ const hackerman = (password) => {
 
 }
 
-const password = "C#d@rH#us@"
-console.log(hackerman(password))
+const hackerman = (password) => {
+    let iv = Buffer.from(password.iv, 'hex')
+    let valor = Buffer.from(password.passwordEncriptado, 'hex')
+
+    let decipher = crypto.createDecipheriv(algoritmo, Buffer.from(key), iv)
+
+    let desencriptar = decipher.update(valor)
+    desencriptar =  decipher.final()
+
+    return desencriptar.toString()
+}
+
+const password = "Francisco!"
+const passwordEncriptado = encriptar(password)
+
+console.log(hackerman(passwordEncriptado))
