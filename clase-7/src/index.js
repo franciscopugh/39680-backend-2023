999faab817c873d5ba027424447026666405f0b4
Francisco Pugh <franciscopugh01@gmail.com>
2023-01-25 23:19:40 -0300

    Clase 7

--- clase-7/src/index.js
+++ clase-7/src/index.js
@@ -15,8 +15,6 @@ const busqueda = async () => {
     }
     
 }
-*/
-
 const http = require('http')
 //import * as http from 'http'
 const PORT = 5000
@@ -27,4 +25,53 @@ const server = http.createServer((request, response) => {
 server.listen(PORT, () => {
     console.log(`Server on port ${PORT}`)
 })
+*/
 
+import express from 'express'
+//const express = require('express')
+const app = express()
+const PORT = 4000
+
+const productos = [
+    {
+        nombre: "Pepitas",
+        id:1,
+        categoria: "Snack"
+    }, 
+    {
+        nombre: "Lentejitas",
+        id:2,
+        categoria: "Legumbre"
+    },
+    {
+        nombre: "Mani",
+        id:3,
+        categoria: "Snack"
+    }
+]
+
+app.use(express.urlencoded({extended: true})) //Permite busquedas de url complejas
+
+app.get('/contacto', (req, res) => {
+    res.send("Hola, esta es la pagina de contacto")
+})
+
+app.get('/producto/:id', async (req, res) => {
+    console.log(productos.find(prod => prod.id === parseInt(req.params.id)))
+    res.send("Hola, esta es la pagina de contacto")
+})
+
+app.get('/producto', (req, res) => {
+
+    let {categoria}= req.query
+    console.log(productos.filter(producto => producto.categoria === categoria))
+    res.send("Producto route")
+})
+
+app.get('/', (req, res) => {
+    res.send("Hola, esta es la pagina de inicio")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`)    
+})
