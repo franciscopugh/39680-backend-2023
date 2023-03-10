de6c148894af2fe7a016fa64a387497e3cac9e9a
Francisco Pugh <franciscopugh01@gmail.com>
2023-03-10 12:39:30 -0300

    Clase 16 modificada

--- clase-16-2/src/dao/daoManager.js
+++ clase-16-2/src/dao/daoManager.js
@@ -1,13 +1,14 @@
 
 
 export const getManagerMessages = async () => {
-    const modelMessage = process.env.SELECTEDBDD === 1 ? await import('./MongoDB/models/Message.js') :
+    const modelMessage = process.env.SELECTEDBDD == 1 ? await import('./MongoDB/models/Message.js') :
         await import('./Postgresql/models/Message.js')
     return modelMessage
 }
 
 export const getManagerProducts = async () => {
-    const modelProduct = process.env.SELECTEDBDD ? await import('./MongoDB/models/Product.js') :
+    const modelProduct = process.env.SELECTEDBDD == 1 ? await import('./MongoDB/models/Product.js') :
         await import('./Postgresql/models/Product.js')
+
     return modelProduct
 }
--- clase-16-2/src/index.js
+++ clase-16-2/src/index.js
@@ -1,12 +1,9 @@
 import "dotenv/config"
 import express from "express";
-import { Socket } from "socket.io";
-import { getManagerMessages } from "./dao/daoManager";
-//import { ManagerMessageMongoDB } from "./dao/MongoDB/models/Message"; NO SE HACE POR QUE DEBO CONSULTAR A DAO
-//const managerMessage = new ManagerMessageMongoDB()
-const app = express()
+import { Server } from "socket.io";
+import { getManagerMessages } from "./dao/daoManager.js";
 
-const managerMessage = new getManagerMessages()
+const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -15,10 +12,12 @@ app.set("port", process.env.PORT || 5000)
 
 const server = app.listen(app.get("port"), () => console.log(`Server on port ${app.get("port")}`))
 
-const io = Socket(server)
+const io = new Server(server)
 
-io.on("connection", (socket) => {
-    socket.on("message", info => {
+io.on("connection", async (socket) => {
+    socket.on("message", async (info) => {
+        const data = await getManagerMessages()
+        const managerMessage = new data.ManagerMessageMongoDB
         managerMessage.addElements([info]).then(() => {
             managerMessage.getElements().then((mensajes) => {
                 console.log(mensajes)
